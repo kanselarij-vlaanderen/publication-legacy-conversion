9677791a2e0096cd38f1812d38389da685bf245d
mdv-professional <mdv.werk@outlook.com>
2021-10-13 16:52:48 +0200

    KAS-2876 pt. 8 remove unused code & script > improvements [pub feedback]

--- config.rb
+++
@@ -1,4 +0,0 @@
-module Config
-  ACCESS_DB_EXPORT = ENV["INPUT_FILE"] || "/data/input/legacy_data.xml"
-  MANDATEES_CORRECTION_PATH = File.join(__dir__, "configuration/mandatees-corrections.csv")
-end
