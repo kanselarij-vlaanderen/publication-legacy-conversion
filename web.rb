d6d2e202c06350c3ccc153a9438ab13ae03f3ef3
mdv-professional <mdv.werk@outlook.com>
2021-12-06 12:51:04 +0100

    KAS-3032 add POST call to match documentation > add undisplayed dates to remark field [pub feedback]

--- web.rb
+++ web.rb
@@ -5,3 +5,14 @@ get '/ingest' do
   
   status 200
 end
+
+post '/ingest' do
+  range = params['range']&.split(',')&.map { |s| s.to_i }
+  if range
+    records = AccessDB.nodes[(range[0]..range[1])]
+  end
+
+  run("/data/input/", "/data/output/", records)
+  
+  status 200
+end
