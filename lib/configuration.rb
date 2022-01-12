f55fa6c45f864eeb2e7cde470a3bd9e4d9759c30
Erika Pauwels <erika.pauwels@gmail.com>
2022-01-12 15:18:09 +0100

    Only support execution inside a docker container

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -1,46 +1,40 @@
 module Configuration
   module Files
     def self.regulation_types_keys
-      path = File.join(__dir__, "../configuration/regulation-types-keys.csv")
-      # relative path does not work in Docker container because of different working directory
-      return CSV.open path, "rt", encoding: "UTF-8" # otherwise Ruby 2.5 assumes ASCII
+      path = File.join("/app/configuration/regulation-types-keys.csv")
+      return CSV.open path, "rt", encoding: "UTF-8"
     end
 
     def self.regulation_types_uris
-      path = File.join(__dir__, "../configuration/regulation-types-uris.csv")
-      # relative path does not work in Docker container because of different working directory
-      return CSV.open path, "rt", encoding: "UTF-8" # otherwise Ruby 2.5 assumes ASCII
+      path = File.join("/app/configuration/regulation-types-uris.csv")
+      return CSV.open path, "rt", encoding: "UTF-8"
     end
 
     def self.government_domains_keys
-      path = File.join(__dir__, "../configuration/government-domains-keys.csv")
-      # relative path does not work in Docker container because of different working directory
-      return CSV.open path, "rt", encoding: "UTF-8" # otherwise Ruby 2.5 assumes ASCII
+      path = File.join("/app/configuration/government-domains-keys.csv")
+      return CSV.open path, "rt", encoding: "UTF-8"
     end
 
     def self.government_domains_uris
-      path = File.join(__dir__, "../configuration/government-domains-uris.csv")
-      # relative path does not work in Docker container because of different working directory
-      return CSV.open path, "rt", encoding: "UTF-8" # otherwise Ruby 2.5 assumes ASCII
+      path = File.join("/app/configuration/government-domains-uris.csv")
+      return CSV.open path, "rt", encoding: "UTF-8"
     end
 
     def self.government_domains_ignore
-      path = File.join(__dir__, "../configuration/government-domains-ignore.csv")
-      # relative path does not work in Docker container because of different working directory
-      return CSV.open path, "rt", encoding: "UTF-8" # otherwise Ruby 2.5 assumes ASCII
+      path = File.join("/app/configuration/government-domains-ignore.csv")
+      return CSV.open path, "rt", encoding: "UTF-8"
     end
 
     def self.mandatees_corrections
-      path = File.join(__dir__, "../configuration/mandatees-corrections.csv")
-      # relative path does not work in Docker container because of different working directory
-      return CSV.open path, "rt", encoding: "UTF-8" # otherwise Ruby 2.5 assumes ASCII
+      path = File.join("/app/configuration/mandatees-corrections.csv")
+      return CSV.open path, "rt", encoding: "UTF-8"
     end
   end
 
   module Output
     def self.government_domains
       output_dir = Configuration::Environment.output_dir
-      file = File.join __dir__, "..", output_dir, "government-domains-mapping.csv"
+      file = File.join output_dir, "government-domains-mapping.csv"
       return CSV.open file, "wt", encoding: "UTF-8"
     end
   end
@@ -51,11 +45,11 @@ module Configuration
     end
 
     def self.input_dir
-      return ENV['INPUT_DIR'] || '/data/input/'
+      return ENV['INPUT_DIR'] || '/data/input'
     end
 
     def self.output_dir
-      return ENV['OUTPUT_DIR'] || '/data/output/'
+      return ENV['OUTPUT_DIR'] || '/data/output'
     end
   end
 end
