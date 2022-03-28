e77a34fd6f0f651c7f6a3b4530366f636affa011
mdv-professional <mdv.werk@outlook.com>
2022-03-28 13:39:13 +0200

    return 400 when no actions are specified / /api KAS-3278 pub: v3.1: government domain full name

--- web.rb
+++ web.rb
@@ -5,6 +5,10 @@ post '/api' do
   take = params['take']&.to_i
   dossiernummer_list = params['dossiernummer']&.split(',')
   actions = Set.new params['actions']&.split(',')
+  if actions.empty?
+    status 400
+    return 'actions query parameter is required: specify validate and/or convert: ?actions=validate,convert'
+  end
 
   if range
     records = AccessDB.nodes[(range[0]..range[1])]
